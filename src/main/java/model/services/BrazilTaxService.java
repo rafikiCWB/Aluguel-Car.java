56652389140bb302983b01b2811d33dbc608c5fd
rafaelgrando <sta_rafinha@hotmail.com>
2021-04-11 01:59:34 -0300

    "Initial project, criated aluguel car is not implements interface "

---
+++ src/main/java/model/services/BrazilTaxService.java
@@ -0,0 +1,13 @@
+package model.services;
+
+public class BrazilTaxService {
+    //utilizado double tipo primitivo pq estou deduzindo que sempre tera um valor aqui; ou seja sempre tera um valor de imposto;
+    public double tax(double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.2;
+        } else {
+            return amount * 0.15;
+        }
+
+    }
+}
